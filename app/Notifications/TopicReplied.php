6ca81488866336e49e1945f4c09e92f31d3160c3
nishihong <249183418@qq.com>
2021-09-24 10:27:44 +0800

    【A】支持队列的邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notification;
 use App\Models\Reply;
 
 class TopicReplied extends Notification
+// class TopicReplied extends Notification implements ShouldQueue  //???redis队列会报错
 {
     use Queueable;
 
@@ -34,7 +35,7 @@ class TopicReplied extends Notification
     public function via($notifiable)
     {
         // 开启通知的频道
-        return ['database'];
+        return ['database', 'mail'];
     }
 
     public function toDatabase($notifiable)
@@ -53,5 +54,14 @@ class TopicReplied extends Notification
             'topic_id' => $topic->id,
             'topic_title' => $topic->title,
         ];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
+        return (new MailMessage)
+            ->line('你的话题有新回复！')
+            ->action('查看回复', $url);
     }
 }
