c333504988e513cbc9b088d458022934bdaab929
nishihong <249183418@qq.com>
2021-09-27 17:24:50 +0800

    【A】活跃用户

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory;
     use HasRoles;
+    use Traits\ActiveUserHelper;
 
     use Notifiable {
         notify as protected laravelNotify;
